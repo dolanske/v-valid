656c3f1c8dfe5423a0c8c4b3707f0dbbeeb07fa9
Jan Dolanský <dolanovsky@gmail.com>
2024-06-22 17:35:11 +0200

    Despair

--- src/core/rules.ts
+++ src/core/rules.ts
@@ -1,35 +1,73 @@
+import type { ComputedRef } from 'vue-demi'
 import { computed, reactive } from 'vue-demi'
+import { isArray } from 'lodash-es'
 import type { ValidationRule, ValidationRuleObject } from '../types'
 import { maxLength } from '../validators/maxLength'
 import { useValidation } from './validate'
 
 type Accepted = ValidationRule | ValidationRuleObject
+type AcceptedRule = Accepted[] | Record<string, Accepted>
 
-export function defineRules<F>(form: F, rules: Partial<Record<keyof F, Accepted>>) {
-  return computed(() => rules)
+// type DeepReplace<T, M> = {
+//   [P in keyof T]:
+//   T[P] extends object
+//     ? DeepReplace<T[P], M>
+//     : AcceptedRule
+// }
+
+// TODO:
+// Deep autocomplete when creating rules
+
+type DeepPartial<T> = T extends keyof T ? {
+  [P in keyof T]: DeepPartial<T[P]>
+} : { [P in keyof T]: AcceptedRule }
+
+export function defineRules<F>(form: F, rules: DeepPartial<F>) {
+  const formattedRules = !isArray(rules)
+    ? rules
+    : rules.reduce((group, item, index) => {
+      if (group[item.name]) {
+        Reflect.set(group, item.name + index, item)
+      }
+      else {
+        Reflect.set(group, item.name, item)
+      }
+      return group
+    }, {})
+
+  return computed(() => formattedRules)
 }
-
-// TODO
-// Fix deeply nested autocomplete
 
 // Test
 const form = reactive({
   name: '',
   age: 0,
   properties: {
-    sex: '',
+    gender: '',
   },
 })
 
 // TODO
-// Support array declaration
+// Support array declaration. This will only be available through defineRules. And its output will still just be object based
+
+// TODO
+// Errors autocomplete must be deep to mimic form
 
 const rules = defineRules(form, {
-  age: [required, maxLength(10)],
+  properties: {
+    gender: {
+      max: maxLength(10),
+    },
+  },
+  name: {
+    max: maxLength(10),
+  },
 })
 
 //
-const form = useValidation(form, rules)
+const { errors } = useValidation(form, rules)
+
+console.log(errors)
 
 // const rules = defineRules<typeof form>({
 //   'age': {
